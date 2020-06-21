79ef27bcfbc69322f181f857c4ef713cacb777f8
Myst33d <myst33d@gmail.com>
2020-06-21 17:18:57 +0600

    Clean up the code

--- akira.py
+++ akira.py
@@ -1,8 +1,8 @@
 import os, json, tempfile, shutil, akira_lang
 from youtube_dl import YoutubeDL
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler
 from telegram.ext.dispatcher import run_async
-from telegram import ChatAction, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ChatAction, ParseMode
 from PIL import Image
 
 akira = "1.01-alpha"
@@ -67,6 +67,7 @@ def akira_yt2a(update, context):
 					thumbext = "jpg"
 					Image.open(f"{temp_dir}/audio-{id}.jpg").convert("RGB").save(f"{temp_dir}/audio-{id}.jpg")
 				else:
+					# This code is questionable
 					thumbext = "webp"
 					Image.open(f"{temp_dir}/audio-{id}.webp").convert("RGB").save(f"{temp_dir}/audio-{id}.jpg")
 				update.message.reply_audio(open(f"{temp_dir}/audio-{id}.{audioext}", "rb"),
@@ -109,34 +110,6 @@ def akira_setlang(update, context):
 		update.message.reply_text(akira_lang.translations
 			[get_lang(context)]["akira_noargs"])
 
-@run_async
-def akira_logger(update, context):
-	# Logging for dev chat(chat id censored on Github)
-	if update.message.chat.id == 0:
-		print(update.message)
-
-@run_async
-def akira_reminder(update, context):
-	# Not yet
-	context.user_data["captcha_" + update.message.chat.username] = \
-		context.job_queue.run_once(akira_debug_job,
-			5,
-			context=(update, context))
-	update.message.reply_text("Job set")
-
-@run_async
-def akira_debug_job(context):
-	# Not yet
-	update, context = context.job.context
-	context.bot.send_message(update.message.chat.id,
-		"Trigger[\u200b](tg://user?id={})".format(update.message.from_user.id),
-		ParseMode.MARKDOWN_V2)
-
-@run_async
-def akira_captcha(update, context):
-	# Not yet
-	pass
-
 if not os.path.exists("settings.json"):
     with open("settings.json", "w") as settings_file:
         settings_file.write(json.dumps(default_settings, indent=4))
@@ -149,8 +122,5 @@ updater.dispatcher.add_handler(CommandHandler("start", akira_start))
 updater.dispatcher.add_handler(CommandHandler("yt2a", akira_yt2a))
 updater.dispatcher.add_handler(CommandHandler("version", akira_version))
 updater.dispatcher.add_handler(CommandHandler("setlang", akira_setlang))
-#updater.dispatcher.add_handler(CommandHandler("debug", akira_debug))
-#updater.dispatcher.add_handler(CommandHandler("captcha", akira_captcha))
-updater.dispatcher.add_handler(MessageHandler(Filters.all, akira_logger))
 updater.start_polling()
 print("Akira started")
