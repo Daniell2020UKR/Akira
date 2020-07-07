b4854faf3064541ed9803e3f9b5c9efd7d5e4252
Myst33d <myst33d@gmail.com>
2020-07-07 19:09:59 +0600

    More core

--- akira.py
+++ akira.py
@@ -1,72 +1,200 @@
 import os, asyncio, random, qrcode, tempfile, shutil, uuid
+from telethon.sessions import MemorySession
+from telethon.tl.types import DocumentAttributeAudio
+from telethon import TelegramClient
 from nanogram import Nanogram
+from pyzbar.pyzbar import decode
+from PIL import Image
+from youtube_dl import YoutubeDL
 
 akira = '0.1'
-
-akira_emotion_list = [
-	'CAACAgEAAxkBAAIFOV8DEObNjlBhpCutq4wXnqbrfjyiAAKxcgACrxliB5TVrEn25LatGgQ', # Happy
-	'CAACAgEAAxkBAAIFOl8DEPRjVNUR-yHQiZqQb3l44N1SAAKycgACrxliB51RDc5Wr2auGgQ', # "Heya!" sticker
-	'CAACAgEAAxkBAAIFO18DEQucDJDuoPXtTld5aHhNgZKiAAKzcgACrxliB8RVY-WhFab7GgQ', # Sad
-	'CAACAgEAAxkBAAIFPF8DERvXwj1MFYl8mIQjGy5AkJOkAAK0cgACrxliBw27i49II75TGgQ', # Excited
-	'CAACAgEAAxkBAAIFPV8DEWMj0anyldeCkqH-vKcQLspIAAK1cgACrxliBz_n4xaSDXdsGgQ', # Confused
-	'CAACAgEAAxkBAAIFPl8DEXari6m0XAmsZnQTJUZZrs7pAAK2cgACrxliB0Nvze67_rYGGgQ', # Blushing
-	'CAACAgEAAxkBAAIFP18DEY8mTCzF91HUtqY1guH4ZJQEAAK3cgACrxliBygb2swZUQI_GgQ', # Extremly blushing
-	'CAACAgEAAxkBAAIFQF8DEai-DSojPkooTXYBW2JYH8lpAAK4cgACrxliB6lN16aqL4XTGgQ', # Yawn
-	'CAACAgEAAxkBAAIFQV8DEb5GbCBRaXCpLVOBHLGh18FAAAK5cgACrxliB7KwP472kZ29GgQ', # Annoyed
-	'CAACAgEAAxkBAAIFQl8DEdOosPJ16inmoF-dAAGWPvC5lQACM3wAAq8ZYgcNwiff8AmGtxoE', # A N G E R Y
-	'CAACAgEAAxkBAAIFQ18DEeW8Tm9FKBF-stW_duOdgVIJAAK6cgACrxliB3-dKoMlIhFWGgQ' # Confident
-]
 
 def log(text): print(f'[Akira] {text}')
 
 log(f'Starting Akira {akira}...')
 
 log('Creating bot client...')
+client = TelegramClient(MemorySession(), os.environ.get('API_ID'), os.environ.get('API_HASH')).start(bot_token=os.environ.get('BOT_TOKEN'))
 bot = Nanogram(os.environ.get('BOT_TOKEN'))
 
 log('Creating temporary directory...')
 temp_dir = tempfile.gettempdir() + '/akira'
-if os.path.exists(temp_dir):
-	shutil.rmtree(temp_dir)
+if os.path.exists(temp_dir): shutil.rmtree(temp_dir)
 os.mkdir(temp_dir)
 
-async def akira_emotion(bot, update):
-	index = random.randint(0, len(akira_emotion_list) - 1)
-	await bot.send_sticker(
-		akira_emotion_list[index],
-		chat_id=update['message']['chat']['id'],
-		reply_to_message_id=update['message']['message_id']
-	)
-
 async def akira_qr(bot, update):
-	if update['message']['args']:
+	if update['message'].get('reply_to_message'):
+		if update['message']['reply_to_message'].get('text') or update['message']['reply_to_message'].get('caption'):
+			text = update['message']['reply_to_message'].get('text') or update['message']['reply_to_message'].get('caption')
+			qrid = uuid.uuid4()
+			qr = qrcode.make(text)
+			qr.save(f'{temp_dir}/qr-{qrid}.jpg')
+			await bot.send_photo(
+				photo=open(f'{temp_dir}/qr-{qrid}.jpg', 'rb'),
+				chat_id=update['message']['chat']['id'],
+				reply_to_message_id=update['message']['message_id']
+			)
+			os.remove(f'{temp_dir}/qr-{qrid}.jpg')
+		elif update['message']['reply_to_message'].get('photo'):
+			file_path = await bot.download_file(temp_dir, update['message']['reply_to_message']['photo'][0]['file_id'])
+			codes = decode(Image.open(file_path))
+			response_text = ''
+			for code in codes:
+				if code.type == 'QRCODE':
+					response_text += 'QR Code:\n'
+				elif code.type == 'EAN13' or code.type == 'CODE128':
+					response_text += 'Bar Code:\n'
+				else:
+					response_text += 'Unknown Code:\n'
+				response_text += '{}\n'.format(code.data.decode('utf-8'))
+			if response_text:
+				await bot.send_message(
+					text=response_text,
+					chat_id=update['message']['chat']['id'],
+					reply_to_message_id=update['message']['message_id']
+				)
+			else:
+				await bot.send_message(
+					text='No QR/bar codes found',
+					chat_id=update['message']['chat']['id'],
+					reply_to_message_id=update['message']['message_id']
+				)
+			os.remove(file_path)
+	elif update['message']['args']:
 		qrid = uuid.uuid4()
 		qr = qrcode.make(' '.join(update['message']['args']))
 		qr.save(f'{temp_dir}/qr-{qrid}.jpg')
 		await bot.send_photo(
-			open(f'{temp_dir}/qr-{qrid}.jpg', 'rb'),
+			photo=open(f'{temp_dir}/qr-{qrid}.jpg', 'rb'),
 			chat_id=update['message']['chat']['id'],
 			reply_to_message_id=update['message']['message_id']
 		)
 		os.remove(f'{temp_dir}/qr-{qrid}.jpg')
+	elif update['message'].get('photo'):
+		file_path = await bot.download_file(temp_dir, update['message']['photo'][0]['file_id'])
+		codes = decode(Image.open(file_path))
+		response_text = ''
+		for code in codes:
+			if code.type == 'QRCODE':
+				response_text += 'QR Code:\n'
+			elif code.type == 'EAN13' or code.type == 'CODE128':
+				response_text += 'Bar Code:\n'
+			else:
+				response_text += 'Unknown Code:\n'
+			response_text += '{}\n'.format(code.data.decode('utf-8'))
+		if response_text:
+			await bot.send_message(
+				text=response_text,
+				chat_id=update['message']['chat']['id'],
+				reply_to_message_id=update['message']['message_id']
+			)
+		else:
+			await bot.send_message(
+				text='No QR/bar codes found',
+				chat_id=update['message']['chat']['id'],
+				reply_to_message_id=update['message']['message_id']
+			)
+		os.remove(file_path)
 	else:
 		await bot.send_message(
-			'No arguments.',
+			text='No arguments.',
+			chat_id=update['message']['chat']['id'],
+			reply_to_message_id=update['message']['message_id']
+		)
+
+async def akira_yt2a(bot, update):
+	if update['message']['args']:
+		if not bot.bot_data.get('ytdl'): bot.bot_data['ytdl'] = {'audio': {}}
+		download_dir = tempfile.mkdtemp(dir=temp_dir)
+		args = {'format': 'bestaudio[ext=m4a][filesize<?250M]', 'outtmpl': f'{download_dir}/audio-%(id)s.%(ext)s', 'writethumbnail': True}
+		sent_message = await bot.send_message(
+			text='Downloading...',
+			chat_id=update['message']['chat']['id'],
+			reply_to_message_id=update['message']['message_id']
+		)
+		try:
+			with YoutubeDL(args) as ydl:
+				audio_info = ydl.extract_info(update['message']['args'][0], download=False)
+				id = audio_info['id']
+				if not bot.bot_data['ytdl']['audio'].get(id):
+					ydl.download([update['message']['args'][0]])
+					if os.path.exists(f'{download_dir}/audio-{id}.webp'): thumbext = 'webp'
+					else: thumbext = 'jpg'
+		except:
+			await bot.send_message(
+				text='Download error',
+				chat_id=update['message']['chat']['id'],
+				reply_to_message_id=update['message']['message_id']
+			)
+			await bot.delete_message(
+				chat_id=update['message']['chat']['id'],
+				message_id=sent_message['message_id']
+			)
+			shutil.rmtree(download_dir)
+			return
+		sent_message = await bot.edit_message(
+			text='Uploading...',
+			chat_id=update['message']['chat']['id'],
+			message_id=sent_message['message_id'],
+		)
+		chat = await client.get_entity(int(update['message']['chat']['id']))
+		try:
+			if bot.bot_data['ytdl']['audio'].get(id):
+				await client.send_file(
+					chat,
+					bot.bot_data['ytdl']['audio'][id],
+					reply_to=int(update['message']['message_id'])
+				)
+			else:
+				audio_file = await client.upload_file(open(f'{download_dir}/audio-{id}.m4a', 'rb'))
+				audio_message = await client.send_file(
+					chat,
+					audio_file,
+					thumb=open(f'{download_dir}/audio-{id}.{thumbext}', 'rb'),
+					reply_to=int(update['message']['message_id']),
+					attributes=[DocumentAttributeAudio(
+						title=audio_info['title'],
+						performer=audio_info['artist'],
+						voice=True,
+						duration=audio_info['duration']
+					)]
+				)
+				bot.bot_data['ytdl']['audio'][id] = audio_message.media
+		except:
+			await bot.send_message(
+				text='Upload error',
+				chat_id=update['message']['chat']['id'],
+				reply_to_message_id=update['message']['message_id']
+			)
+			await bot.delete_message(
+				chat_id=update['message']['chat']['id'],
+				message_id=sent_message['message_id']
+			)
+			shutil.rmtree(download_dir)
+			return
+		await bot.delete_message(
+			chat_id=update['message']['chat']['id'],
+			message_id=sent_message['message_id']
+		)
+		shutil.rmtree(download_dir)
+	else:
+		await bot.send_message(
+			text='No arguments.',
 			chat_id=update['message']['chat']['id'],
 			reply_to_message_id=update['message']['message_id']
 		)
 
 async def akira_start(bot, update):
 	await bot.send_message(
-		'Hi! Im Akira.',
+		text='Hi! Im Akira.',
 		chat_id=update['message']['chat']['id'],
 		reply_to_message_id=update['message']['message_id']
 	)
 
-bot.add_command('/emotion', akira_emotion)
 bot.add_command('/start', akira_start)
 bot.add_command('/qr', akira_qr)
+bot.add_command('/yt2a', akira_yt2a)
 bot.delete_webhook()
-bot.set_webhook(os.environ.get('URL'), os.environ.get('PORT'))
+bot.set_webhook(os.environ.get('URL'), os.environ.get('PORT'), heroku=True)
 log('Started.')
 bot.start_webhook()
--- nanogram.py
+++ nanogram.py
@@ -1,7 +1,7 @@
 # Nanogram 0.1
 # Minimal Telegram Bot API library
 # Dependencies: aiohttp, pyOpenSSL
-import aiohttp, asyncio, ssl, OpenSSL
+import aiohttp, asyncio, ssl, OpenSSL, re, uuid
 from aiohttp import web
 
 class Nanogram:
@@ -10,6 +10,10 @@ class Nanogram:
 		self.session = aiohttp.ClientSession()
 		self.loop = asyncio.get_event_loop()
 		self.commands = {}
+		self.regex = []
+		self.chat_data = {}
+		self.user_data = {}
+		self.bot_data = {}
 		self.me = self.loop.run_until_complete(self.api_request('getMe'))
 
 	def stringizer(self, some_dict):
@@ -44,50 +48,80 @@ class Nanogram:
 	async def process_update(self, update):
 		update = self.stringizer(update)
 		if update.get('message'):
-			if update.get('message').get('text'):
-				update['message']['args'] = update['message']['text'].split(' ')
+			chat_id = update['message']['chat']['id']
+			if chat_id not in self.chat_data.keys():
+				self.chat_data[chat_id] = {}
+			if update.get('message').get('text') or update.get('message').get('caption'):
+				text = update['message'].get('text') or update['message'].get('caption')
+				update['message']['args'] = text.split(' ')
 				command = update['message']['args'].pop(0).split('@')
+				for regex in self.regex:
+					if regex[0].match(update['message']['text']):
+						asyncio.create_task(regex[1](self, update))
+				if self.chat_data[chat_id].get('regex'):
+					for regex in self.chat_data[chat_id]['regex']:
+						if regex[0].match(update['message']['text']):
+							asyncio.create_task(regex[1](self, update))
 				if command[0] in self.commands.keys():
 					if len(command) == 2 and self.me.get('username') in command:
 						asyncio.create_task(self.commands[command[0]](self, update))
 					elif len(command) == 1 and self.me.get('username') not in command:
 						asyncio.create_task(self.commands[command[0]](self, update))
 
-	async def send_message(self, text, **kwargs):
-		request = {'text': text}
-		request.update(kwargs)
-		return await self.api_request('sendMessage', postdata=request)
+	async def send_message(self, **kwargs):
+		return await self.api_request('sendMessage', postdata=kwargs)
 
-	async def send_photo(self, photo, **kwargs):
-		request = {'photo': photo}
-		request.update(kwargs)
-		return await self.api_request('sendPhoto', postdata=request)
+	async def send_photo(self, **kwargs):
+		return await self.api_request('sendPhoto', postdata=kwargs)
 
-	async def send_audio(self, audio, **kwargs):
-		request = {'audio': audio}
-		request.update(kwargs)
-		return await self.api_request('sendAudio', postdata=request)
+	async def send_audio(self, **kwargs):
+		return await self.api_request('sendAudio', postdata=kwargs)
 
-	async def send_sticker(self, sticker, **kwargs):
-		request = {'sticker': sticker}
-		request.update(kwargs)
-		return await self.api_request('sendSticker', postdata=request)
+	async def send_sticker(self, **kwargs):
+		return await self.api_request('sendSticker', postdata=kwargs)
 
-	async def edit_message(self, text, **kwargs):
-		request = {'text': text}
-		request.update(kwargs)
-		return await self.api_request('editMessageText', postdata=request)
+	async def edit_message(self, **kwargs):
+		return await self.api_request('editMessageText', postdata=kwargs)
+
+	async def delete_message(self, **kwargs):
+		return await self.api_request('deleteMessage', postdata=kwargs)
+
+	async def download_file(self, target, file_id):
+		file_info = await self.api_request('getFile', postdata={'file_id': file_id})
+		file_path = file_info['file_path']
+		file_name = file_info['file_path'].split('/')[-1]
+		file_uuid = uuid.uuid4()
+		async with self.session.get(f'https://api.telegram.org/file/bot{self.token}/{file_path}') as response:
+			with open(f'{target}/{file_uuid}-{file_name}', 'wb') as f: f.write(await response.read())
+		return f'{target}/{file_uuid}-{file_name}'
 
 	def add_command(self, command, function):
 		self.commands.update({command: function})
 
-	def set_webhook(self, url, port):
+	def add_regex(self, regex, function):
+		comp = re.compile(regex, re.IGNORECASE)
+		self.regex.append((comp, function))
+
+	def add_regex_chat(self, regex, function, chat_id):
+		comp = re.compile(regex, re.IGNORECASE)
+		if self.chat_data[chat_id].get('regex'):
+			self.chat_data[chat_id]['regex'].append((comp, function))
+		else:
+			self.chat_data[chat_id]['regex'] = []
+			self.chat_data[chat_id]['regex'].append((comp, function))
+
+	def set_webhook(self, url, port, heroku=False):
 		self.webhook_port = port
-		self.generate_ssl_certificate(url)
+		self.heroku = heroku
+		if not self.heroku:
+			self.generate_ssl_certificate(url)
 		async def webhook(request):
 			await self.process_update(await request.json())
 			return web.Response(text='ACK')
-		self.loop.run_until_complete(self.api_request('setWebhook', postdata={'url': f'https://{url}:{port}/{self.token}', 'certificate': open('nanogram.crt', 'rb'), 'max_connections': '100'}))
+		if not self.heroku:
+			self.loop.run_until_complete(self.api_request('setWebhook', postdata={'url': f'https://{url}:{port}/{self.token}', 'certificate': open('nanogram.crt', 'rb'), 'max_connections': '100'}))
+		else:
+			self.loop.run_until_complete(self.api_request('setWebhook', postdata={'url': f'https://{url}/{self.token}', 'max_connections': '100'}))
 		self.app = web.Application()
 		self.app.router.add_post(f'/{self.token}', webhook)
 
@@ -95,6 +129,9 @@ class Nanogram:
 		self.loop.run_until_complete(self.api_request('deleteWebhook'))
 
 	def start_webhook(self):
-		context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-		context.load_cert_chain('nanogram.crt', 'nanogram.prv')
-		web.run_app(self.app, port=self.webhook_port, ssl_context=context)
+		if not self.heroku:
+			context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+			context.load_cert_chain('nanogram.crt', 'nanogram.prv')
+			web.run_app(self.app, port=self.webhook_port, ssl_context=context)
+		else:
+			web.run_app(self.app, port=self.webhook_port)
