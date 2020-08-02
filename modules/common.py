3aef457cfad08eca0b719a0d5c0c38be97bf45a4
Myst33d <myst33d@gmail.com>
2020-08-02 16:30:21 +0600

    Try resolving the file

--- modules/common.py
+++ modules/common.py
@@ -2,17 +2,21 @@ from aiogram import types
 from telethon.utils import resolve_bot_file_id
 
 async def akira_start(message: types.Message):
-	
 	await message.reply("Hi! Im Akira.")
 
 async def akira_yt2a(message: types.Message):
 	await message.reply("This command is not available now.")
 
 async def akira_ipfs(message: types.Message):
-	print(message)
-	await message.reply("This command is not available now.")
-	print(message.text)
-	print(message["text"])
+	if message.reply_to_message:
+		if message.reply_to_message.document:
+			resolved_id = resolve_bot_file_id(message.reply_to_message.document.file_id)
+			print(resolved_id)
+			await message.reply("This command is not available now.")
+		else:
+			await message.reply("Please respond to a message with a file.")
+	else:
+		await message.reply("Please respond to a message with a file.")
 
 def register():
 	dp.register_message_handler(akira_start, commands=["start"], run_task=True)
