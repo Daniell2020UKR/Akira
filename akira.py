e85744225ade3d04f598cd6e4858397d1f95a020
Myst33d <myst33d@gmail.com>
2020-08-13 22:59:39 +0600

    xdl: add more better downloader code

--- akira.py
+++ akira.py
@@ -1,15 +1,14 @@
-import os, tempfile, shutil, aiohttp, aiofiles, urllib
+import os, shutil, aiohttp, aria2p, time
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from telethon.tl.types import MessageMediaDocument, MessageMediaPhoto, DocumentAttributeVideo
+from telethon.tl.types import DocumentAttributeVideo
 from telethon.sessions import MemorySession
 from telethon.utils import get_message_id
 from telethon import TelegramClient
 
 akira = "0.1"
-akira_dir = os.getcwd() + "/akira"
 
 def log(text): print(f"[Akira] {text}")
 
@@ -17,55 +16,23 @@ client = TelegramClient(MemorySession(), os.environ.get("API_ID"), os.environ.ge
 bot = Bot(token=os.environ.get("BOT_TOKEN"))
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+dots = {
+	0: "○○○○○○○○○○",
+	10: "●○○○○○○○○○",
+	20: "●●○○○○○○○○",
+	30: "●●●○○○○○○○",
+	40: "●●●●○○○○○○",
+	50: "●●●●●○○○○○",
+	60: "●●●●●●○○○○",
+	70: "●●●●●●●○○○",
+	80: "●●●●●●●●○○",
+	90: "●●●●●●●●●○",
+	100: "●●●●●●●●●●"
+}
+
 @dp.message_handler(commands=["start"], run_task=True)
 async def akira_start(message: types.Message):
 	await message.reply("Hi! Im Akira.")
-
-@dp.message_handler(commands=["ipfs"], run_task=True)
-async def akira_ipfs(message: types.Message):
-	temp_dir = tempfile.mkdtemp(dir=akira_dir)
-	chat = await client.get_entity(message.chat.id)
-	telethon_message = await client.get_messages(chat, ids=message.message_id)
-	if telethon_message.reply_to_msg_id:
-		telethon_reply_message = await client.get_messages(chat, ids=telethon_message.reply_to_msg_id)
-		if type(telethon_reply_message.media) == MessageMediaDocument:
-			file_size = telethon_reply_message.document.size
-		elif type(telethon_reply_message.media) == MessageMediaPhoto:
-			file_size = telethon_reply_message.photo.size
-		else:
-			await message.reply("Invalid file type.")
-			shutil.rmtree(temp_dir)
-			return
-		if file_size > (512 * 1024 * 1024):
-			await message.reply("File is too big. Maximum file size is 512 MB.")
-			shutil.rmtree(temp_dir)
-			return
-		reply = await message.reply("Downloading...")
-		try:
-			downloaded_file = await telethon_reply_message.download_media(temp_dir)
-		except:
-			await reply.delete()
-			await message.reply("An error occurred while downloading a file.")
-			shutil.rmtree(temp_dir)
-			return
-		await reply.edit_text("Uploading...")
-		try:
-			async with aiohttp.ClientSession() as session:
-				response = await session.post("https://ipfsupload.herokuapp.com/upload", data={"file": open(downloaded_file, "rb")})
-		except:
-			await reply.delete()
-			await message.reply("An error occurred while uploading a file.")
-			shutil.rmtree(temp_dir)
-			return
-		await reply.delete()
-		await message.reply(await response.text())
-	else:
-		await message.reply("Please respond to a message with a file.")
-	shutil.rmtree(temp_dir)
-
-@dp.message_handler(commands=["yt2a"], run_task=True)
-async def akira_yt2a(message: types.Message):
-	await message.reply("This command is not available now.")
 
 @dp.message_handler(commands=["weather"], run_task=True)
 async def akira_weather(message: types.Message):
@@ -77,105 +44,96 @@ async def akira_qr(message: types.Message):
 
 @dp.message_handler(commands=["xdl"], run_task=True)
 async def akira_xdl(message: types.Message):
-	temp_dir = tempfile.mkdtemp(dir=akira_dir)
 	args = message.get_args().split(" ")
-	chat = await client.get_entity(message.chat.id)
-	telethon_message = await client.get_messages(chat, ids=message.message_id)
-	async def default_upload(sent, total):
-		percent = int(round((sent / total) * 100))
-		try:
-			if percent == 20:
-				await reply.edit_text("Uploading... (This might take a while)\n●○○○○")
-			elif percent == 40:
-				await reply.edit_text("Uploading... (This might take a while)\n●●○○○")
-			elif percent == 60:
-				await reply.edit_text("Uploading... (This might take a while)\n●●●○○")
-			elif percent == 80:
-				await reply.edit_text("Uploading... (This might take a while)\n●●●●○")
-		except: pass
-	async def default_download(sent, total):
-		percent = int(round((sent / total) * 100))
-		try:
-			if percent == 20:
-				await reply.edit_text("Downloading...\n●○○○○")
-			elif percent == 40:
-				await reply.edit_text("Downloading...\n●●○○○")
-			elif percent == 60:
-				await reply.edit_text("Downloading...\n●●●○○")
-			elif percent == 80:
-				await reply.edit_text("Downloading...\n●●●●○")
-		except: pass
-	if args:
-		if args[0] == "animekisa":
-			reply = await message.reply("Parsing Fembed ID...")
-			async with aiohttp.ClientSession() as session:
-				try:
-					async with session.get(args[1]) as site:
-						fembed_id = None
-						async for line in site.content:
-							line = line.decode("UTF-8")
-							if "var Fembed" in line and "var Fembed2" not in line:
-								fembed_id = line.split("\"")[1].split("/")[-1]
-				except:
-					await reply.delete()
-					await message.reply("Failed to parse Fembed ID.")
-					return
+	if args[0]:
+		temp_dir = tempfile.mkdtemp(dir=akira_dir)
+		chat = await client.get_entity(message.chat.id)
+		telethon_message = await client.get_messages(chat, ids=message.message_id)
 
-				try:
-					async with session.post(f"https://fcdn.stream/api/source/{fembed_id}") as api:
-						videos = (await api.json())["data"]
-				except:
-					await reply.delete()
-					await message.reply("Failed to get a response from API.")
-					return
+		async def upload_callback(sent, total):
+			percent = int((sent / total) * 100)
+			try:
+				await reply.edit_text("Uploading...\nProgress: {}".format(dots[percent]))
+			except: pass
+		async def download_callback(percent, eta, size, speed):
+			try:
+				await reply.edit_text("Downloading...\nSize: {}\nETA: {}\nSpeed: {}\nProgress: {}".format(size, eta, speed, dots[percent]))
+			except: pass
 
-				index = -1
-				await reply.edit_text("Downloading...\n○○○○○")
-				while True:
-					with urllib.request.urlopen(videos[index]["file"]) as video:
-						video_size = int(video.info()["Content-Length"])
-						if (video_size / 1048576) > 2048:
-							index -= 1
-							if len(videos) + (index + 1) == 0:
-								await reply.delete()
-								await message.reply("Video is too big.")
-								return
-							continue
-						async with aiofiles.open(f"{temp_dir}/video.mp4", "wb") as ovideo:
-							while True:
-								await default_download(await ovideo.tell(), video_size)
-								chunk = video.read(65535)
-								if not chunk:
-									break
-								await ovideo.write(chunk)
-							break
-				await reply.edit_text("Uploading... (This might take a while)\n○○○○○")
-				await client.send_file(
-					chat,
-					file=open(f"{temp_dir}/video.mp4", "rb"),
-					reply_to=telethon_message,
-					progress_callback=default_upload,
-					attributes=[DocumentAttributeVideo(
-						duration=0,
-						w=0,
-						h=0,
-						round_message=False,
-						supports_streaming=True
-					)]
-				)
+		reply = await message.reply("Downloading...\nProgress: {}".format(dots[0]))
+		try:
+			ret = await xdl.downloaders[args[0]](client, args[1], temp_dir, download_callback)
+		except:
+			await message.reply("Downloader \"{}\" is not found.".format(args[0]))
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+		if ret == xdl.xdl_file_too_big:
+			await message.reply("The file is too big.")
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+		elif ret == xdl.xdl_download_error:
+			await message.reply("An error occurred while downloading file.")
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+		elif ret == xdl.xdl_parse_error:
+			await message.reply("An error occurred while parsing info.")
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+		elif ret == xdl.xdl_api_error:
+			await message.reply("An error occurred while calling API.")
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+
+		if ret[0] == xdl.xdl_aria2:
+			target = temp_dir + ret[1].name
+			ext = target.split(".")[-1]
+
+		if ext == "mp4":
+			attrib = [DocumentAttributeVideo(
+				duration=0,
+				w=0,
+				h=0,
+				round_message=False,
+				supports_streaming=True
+			)]
 		else:
-			await message.reply("Unknown downloader \"{}\".".format(args[0]))
+			await message.reply("Unknown file format.")
+			await reply.delete()
+			shutil.rmtree(temp_dir)
+			return
+		await reply.edit_text("Uploading...\nProgress: {}".format(dots[0]))
+		await client.send_file(
+			chat,
+			file=open(target, "rb"),
+			reply_to=telethon_message,
+			progress_callback=upload_callback,
+			attributes=attrib
+		)
+		await reply.delete()
+		shutil.rmtree(temp_dir)
 	else:
 		await message.reply("Usage: /xdl (downloader) (URL)")
-	shutil.rmtree(temp_dir)
 
 if __name__ == "__main__":
 	log(f"Starting Akira {akira}...")
 
+	log("Starting Aria2 daemon...")
+	aria2proc = subprocess.Popen(["aria2c", "--enable-rpc"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+	time.sleep(1)
+
+	log("Creating Aria2 client...")
+	client = aria2p.API(aria2p.Client(host="http://127.0.0.1", port=6800))
+
 	if not os.path.exists(akira_dir):
 		os.mkdir(akira_dir)
 
-	async def on_startup(dp): await bot.set_webhook(os.environ.get("URL") + "/" + os.environ.get("BOT_TOKEN"))
+	async def on_startup(dp):
+		await bot.set_webhook(os.environ.get("URL") + "/" + os.environ.get("BOT_TOKEN"))
 
 	log("Started.")
 	start_webhook(
---
+++ xdl.py
@@ -0,0 +1,52 @@
+import os, subprocess, aiohttp, tempfile, aria2p, urllib, asyncio
+
+xdl_file_too_big = "XDL_FILE_TOO_BIG"
+xdl_download_error = "XDL_DOWNLOAD_ERROR"
+xdl_parse_error = "XDL_PARSE_ERROR"
+xdl_api_error = "XDL_API_ERROR"
+xdl_aria2 = "XDL_ARIA2"
+xdl_path = "XDL_PATH"
+xdl_ytdl = "XDL_YTDL"
+
+async def xdl_animekisa(client, url, output_dir, callback, maxsize=2048):
+	async with aiohttp.ClientSession() as session:
+		try:
+			async with session.get(url) as site:
+				fembed_id = None
+				async for line in site.content:
+					line = line.decode("UTF-8")
+					if "var Fembed" in line and "var Fembed2" not in line:
+						fembed_id = line.split("\"")[1].split("/")[-1]
+		except:
+			return xdl_parse_error
+
+		if fembed_id:
+			try:
+				async with session.post(f"https://fcdn.stream/api/source/{fembed_id}") as api:
+					videos = (await api.json())["data"]
+			except:
+				return xdl_api_error
+
+			index = -1
+			while True:
+				with urllib.request.urlopen(videos[index]["file"]) as video:
+					video_size = int(video.info()["Content-Length"])
+					if (video_size / 1048576) > maxsize:
+						index -= 1
+						if len(videos) + (index + 1) == 0:
+							return xdl_file_too_big
+						continue
+
+					download = client.add_uris([videos[index]["file"]], options={"dir": output_dir})
+					while not download.is_complete and not download.has_failed:
+						download.update()
+						await callback(int(download.progress), download.eta_string(), download.total_length_string(), download.download_speed_string())
+						await asyncio.sleep(1)
+					download.update()
+					return [xdl_aria2, download]
+		else:
+			return xdl_parse_error
+
+downloaders = {
+	"animekisa": xdl_animekisa
+}
