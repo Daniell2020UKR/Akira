1f1d24ae98b1f7ef59898d87913020a18e20c6e3
Myst33d <myst33d@gmail.com>
2020-10-26 13:39:12 +0600

    Piss

--- akira.py
+++ akira.py
@@ -47,7 +47,7 @@ async def upload_callback(sent, total):
 		pass
 
 async def download_callback(percent, eta, size, speed):
-	print(percent)
+	global reply
 	try:
 		if percent in dots.keys():
 			await reply.edit_text("Downloading...\nSize: {}\nETA: {}\nSpeed: {}\nProgress: {}".format(size, eta, speed, dots[percent]))
